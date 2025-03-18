e59b48056cb8d44001f3041db41e2d4c590cd121
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-18 18:44:01 +0100

    Arrays Exercise 1

---
+++ src/main/java/com/agnes/Main.java
@@ -0,0 +1,12 @@
+package com.agnes;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {11, 23, 39};
+        int i;
+        for (i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+
+        }
+    }
+}
