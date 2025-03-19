801f925d9c797e3753a1032f92523d9e3104c7e7
AgnesNazie <cheagnesmbo@gmail.com>
2025-03-19 10:28:08 +0100

    completed exercise 5 and 6 of the Arrays exercises

--- src/main/java/com/agnes/Main.java
+++ src/main/java/com/agnes/Main.java
@@ -8,8 +8,11 @@ public class Main {
         exercise2();
         exercise3();
         exercise4();
+        exercise5();
+        exercise6();
     }
-// exercise 1
+
+    // exercise 1
     public static void exercise1() {
         System.out.println("EXERCISE 1");
         int[] numbers = {11, 23, 39};
@@ -18,15 +21,17 @@ public class Main {
         }
         System.out.println("\n");
     }
+
     // exercise 2. indexOf method
-    public static void exercise2 () {
+    public static void exercise2() {
         System.out.println("EXERCISE 2");
-        int [] numbers = { 1, 3, 5, 7, 9};
+        int[] numbers = {1, 3, 5, 7, 9};
         int index = indexOf(numbers, 5);
         System.out.println("index position of numbers 5 is : " + index);
     }
+
     // create a method to find the indexOf
-    public static int indexOf(int[] array , int value) {
+    public static int indexOf(int[] array, int value) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] == value) {
                 return i;
@@ -46,21 +51,41 @@ public class Main {
         Arrays.sort(cities);
         System.out.println("SORTED STRING ARRAY:" + Arrays.toString(cities));
     }
+
     public static void exercise4() {
         System.out.println("EXERCISE 4: ");
-        int [] firstNumbers = {1, 15, 20 };
-        int [] secondNumbers = new int [firstNumbers.length];
-        for (int i = 0; i <firstNumbers.length; i++) {
+        int[] firstNumbers = {1, 15, 20};
+        int[] secondNumbers = new int[firstNumbers.length];
+        for (int i = 0; i < firstNumbers.length; i++) {
             secondNumbers[i] = firstNumbers[i];
         }
         System.out.println("Elements from first Numbers: ");
-        for(int num : firstNumbers) {
+        for (int num : firstNumbers) {
             System.out.print(num + " ");
         }
         System.out.println("\nElements from second Numbers: ");
-        for(int num : secondNumbers) {
+        for (int num : secondNumbers) {
             System.out.print(num + " ");
         }
-        // exercise 5. String Array
+    }
+    // exercise 5. String Array
+
+    public static void exercise5() {
+        System.out.println("\nEXERCISE 5: ");
+        String[][] countryCities = {{"France", "Paris"}, {"Sweden", "Stockholm"}};
+        for (int i = 0; i < countryCities.length; ++i) {
+            System.out.println(countryCities[i][0] + " " + countryCities[i][1]);
+        }
+    }
+    //exercise 6.find average of 6 numbers
+    public static void exercise6(){
+        System.out.println("\nEXERCISE 6: ");
+        int [] numbers = {43, 5, 23, 17,2, 14};
+        int sum = 0;
+        for (int num : numbers) {
+            sum +=num;
+        }
+        double average = (double) sum / numbers.length;
+        System.out.println("Average is: " + average);
     }
 }
